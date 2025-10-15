e4d6b6abbaad8a67d89a81a93a21f3e5851b7a3d
n1k0t0m4c <niko.tomac@student.um.si>
2025-10-15 19:35:57 +0200

    Dodan program

---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Zdravo!!
+echo se ti zdi da program zanimiv? y/n
+set /p input=COMMAND?
+if %input% == n start https://media.tenor.com/u6002ayqdi4AAAAe/good-morning.png
+if %input% == y start https://assets.iflscience.com/assets/articleNo/37244/aImg/13729/1470396368-cover-image-o.webp
